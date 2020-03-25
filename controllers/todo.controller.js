e62feb774158ad921bdcc4ce58fad46730b88cab
felkholy <felkholy@iotblue.net>
2020-03-25 15:15:23 +0200

    unit test for Http POST /todos

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,1 +1,5 @@
-exports.createTodo = ()=>{};
+const todoModel = require("../model/todo.model")
+exports.createTodo = (req, res, next)=>{
+    const createTodo = todoModel.create(req.body);
+    res.status(201).json(createTodo);
+};
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,13 +1,45 @@
 const todoController = require("../../controllers/todo.controller");
-
 const todoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
 
 todoModel.create = jest.fn();
 
-describe("todoController.createTodo",()=>{
-    it("should have a createTodo function",()=>{
+let req, res, next;
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
+
+describe("todoController.createTodo", () => {
+
+    beforeEach(() => {
+        req.body = newTodo;
+    })
+    
+    it("should have a createTodo function", () => {
         expect(typeof todoController.createTodo).toBe("function");
     });
+
+    it("should call todoModel.create", () => {
+        todoController.createTodo(req, res, next);
+        expect(todoModel.create).toBeCalledWith(newTodo);
+    })
+
+    it("should return 201 response code", () => {
+        todoController.createTodo(req, res, next);
+        expect(res.statusCode).toBe(201);
+        expect(res._isEndCalled).toBeTruthy();
+    })
+
+    it("should return json body response", () => {
+        todoModel.create.mockReturnValue(newTodo);
+        todoController.createTodo(req, res, next);
+        // expect(res.body).
+        expect(res._getJSONData()).toStrictEqual(newTodo);
+    })
+
 });
 
 
